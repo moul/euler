efe3af59e7ab0d9f897e252fe9f0f5b276a8c85e
Manfred Touron <m@42.am>
2015-11-05 23:26:49 +0100

    Challenge 1

---
+++ challenges/c001.go
@@ -0,0 +1,20 @@
+package challenges
+
+func init() {
+	Challenges[1] = Challenge{
+		Title:    "Multiples of 3 and 5",
+		Correct:  true,
+		Callback: c001,
+		ID:       1,
+	}
+}
+
+func c001() (interface{}, error) {
+	sum := 0
+	for i := 3; i < 1000; i++ {
+		if i%3 == 0 || i%5 == 0 {
+			sum += i
+		}
+	}
+	return sum, nil
+}
