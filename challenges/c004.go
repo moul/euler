e2aa497de562de368b56cafae8f9d9e9e711ba8a
Manfred Touron <m@42.am>
2015-11-05 23:26:58 +0100

    Challenge 4

---
+++ challenges/c004.go
@@ -0,0 +1,37 @@
+package challenges
+
+import (
+	"strconv"
+
+	"github.com/moul/euler/utils"
+)
+
+func init() {
+	Challenges[4] = Challenge{
+		Title:    "Largest palindrome product",
+		Correct:  true,
+		Callback: c004,
+		ID:       4,
+	}
+}
+
+func c004() (interface{}, error) {
+	biggest := 0
+
+	for a := 999; a >= 100; a-- {
+		for b := 999; b >= 100; b-- {
+			product := a * b
+			if product < biggest {
+				continue
+			}
+			productStr := strconv.Itoa(product)
+			if productStr == utils.ReverseString(productStr) {
+				if biggest < product {
+					biggest = product
+				}
+			}
+		}
+	}
+
+	return biggest, nil
+}
