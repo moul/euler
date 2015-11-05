84a2c5d2f674cf9fa0e696b4aaba6c6064803558
Manfred Touron <m@42.am>
2015-11-05 23:26:52 +0100

    Challenge 2

---
+++ challenges/c002.go
@@ -0,0 +1,27 @@
+package challenges
+
+func init() {
+	Challenges[2] = Challenge{
+		Title:    "Even Fibonacci numbers",
+		Correct:  true,
+		Callback: c002,
+		ID:       2,
+	}
+}
+
+func c002() (interface{}, error) {
+	sum := 0
+
+	left := 1
+	right := 2
+	for left < 4000000 {
+		if right%2 == 0 {
+			sum += right
+		}
+		next := right + left
+		left = right
+		right = next
+	}
+
+	return sum, nil
+}
