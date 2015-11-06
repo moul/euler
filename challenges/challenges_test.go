7a0bad26447af11aec95a89dfda67b5965d0f0b8
Manfred Touron <m@42.am>
2015-11-06 20:25:19 +0100

    Add tests

--- challenges/challenges_test.go
+++ challenges/challenges_test.go
@@ -26,10 +26,34 @@ func ExampleC004() {
 	// Output: 906609
 }
 
+func ExampleC005() {
+	result, _ := C005()
+	fmt.Println(result)
+	// Output: 232792560
+}
+
+func ExampleC006() {
+	result, _ := C006()
+	fmt.Println(result)
+	// Output: 25164150
+}
+
 func ExampleC007() {
 	result, _ := C007()
 	fmt.Println(result)
 	// Output: 104743
+}
+
+func ExampleC008() {
+	result, _ := C008()
+	fmt.Println(result)
+	// Output: 23514624000
+}
+
+func ExampleC009() {
+	result, _ := C009()
+	fmt.Println(result)
+	// Output: 31875000
 }
 
 /*
@@ -39,3 +63,15 @@ func ExampleC026() {
 	// Output: XXX
 }
 */
+
+func ExampleC031() {
+	result, _ := C031()
+	fmt.Println(result)
+	// Output: 73682
+}
+
+func ExampleC206() {
+	result, _ := C206()
+	fmt.Println(result)
+	// Output: 1389019170
+}
