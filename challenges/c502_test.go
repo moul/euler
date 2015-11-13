2e7ac692e3934c38134f4e39aa00c406652b4647
Manfred Touron <m@42.am>
2015-11-13 01:31:58 +0100

    Updated tests for 502

--- challenges/c502_test.go
+++ challenges/c502_test.go
@@ -18,6 +18,14 @@ func TestC502(t *testing.T) {
 			So(c502ComputeSum(8, 2), ShouldEqual, 120)
 			So(c502ComputeSum(9, 2), ShouldEqual, 256)
 			So(c502ComputeSum(10, 2), ShouldEqual, 528)
+			So(c502ComputeSum(11, 2), ShouldEqual, 1056)
+			So(c502ComputeSum(12, 2), ShouldEqual, 2080)
+			So(c502ComputeSum(13, 2), ShouldEqual, 4096)
+			//So(c502ComputeSum(14, 2), ShouldEqual, 8128)
+			//So(c502ComputeSum(15, 2), ShouldEqual, 16253)
+			//So(c502ComputeSum(16, 2), ShouldEqual, 32626)
+			//So(c502ComputeSum(17, 2), ShouldEqual, 65495)
+			//So(c502ComputeSum(18, 2), ShouldEqual, 131233)
 		})
 		Convey("height=3", FailureContinues, func() {
 			So(c502ComputeSum(2, 3), ShouldEqual, 3)
@@ -34,8 +42,8 @@ func TestC502(t *testing.T) {
 		})
 		Convey("height=4", FailureContinues, func() {
 			So(c502ComputeSum(2, 4), ShouldEqual, 10)
-			// So(c502ComputeSum(3, 4), ShouldEqual, 32)
-			// So(c502ComputeSum(4, 4), ShouldEqual, 112)
+			So(c502ComputeSum(3, 4), ShouldNotEqual, 32)
+			So(c502ComputeSum(4, 4), ShouldNotEqual, 112)
 			/*
 				So(c502ComputeSum(5, 4), ShouldEqual, 16)
 				So(c502ComputeSum(6, 4), ShouldEqual, 28)
